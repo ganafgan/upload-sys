7a79005a1c6e40bd7837c7597ffd58c13d7726e4
afgantaufiq <ganafgan@gmail.com>
2020-04-21 18:57:44 +0700

    update

---
+++ precache-manifest.10de84033f560c87ccfcd97b0b5ba009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b0b6fbcba18b0ea012b198b85da76c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2bd6d4cf789eeb782eb",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f2bd6d4cf789eeb782eb",
+    "url": "/static/js/2.96ef981f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.96ef981f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e7e928b674e6005e3d",
+    "url": "/static/js/main.9056ced3.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
